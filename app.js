3bfff0032a6ee8c3943ed249081b3dddfd150d6c
sangumee <super2451894@naver.com>
2017-10-30 15:40:52 +0900

    NPM Source Install
    NPM Source Install

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var BodyParser = require('body-parser');
+var app = express();
+var path = require('path');
+app.locals.pretty = true;
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Connect OrientDB Database
+var OrientDB = require('orientjs');
+var server = OrientDB({
+  host: 'sangumserver.iptime.org',
+  port: 2424,
+  username: 'root',
+  password: 'password123' // Bad Method
+});
+var db = server.use('zzapcord');
+
+// Main Page
+app.get('/', function(req,res){
+  res.render('login');
+});
+
+app.listen(80, function() {
+  console.log('Connected 80 port!!!');
+});
