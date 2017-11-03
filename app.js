cc2c3e944ddac118ec3f882af8aa8d65a0f1fb0b
sangumee <super2451894@naver.com>
2017-11-04 02:29:14 +0900

    Game_list Finish

--- app.js
+++ app.js
@@ -26,8 +26,19 @@ app.get('/', function(req,res){
 });
 
 // Main Page , After Login
-app.get('/main', function(req,res){
-  res.render('main');
+app.get(['/main','/main/:id'], function(req,res){
+  var sql='SELECT FROM game_list';
+  db.query(sql).then(function(game_lists){
+    var id=req.params.id;
+    if(id){
+      var sql='SELECT FROM game_list WHERE @rid=:rid';
+      db.query(sql,{params:{rid:id}}).then(function(game_list){
+        res.render('main', {game_lists:game_lists, game_list:game_list[0]});
+      });
+    }else{
+      res.render('main', {game_lists:game_lists});
+    }
+  })
 });
 
 app.listen(80, function() {
