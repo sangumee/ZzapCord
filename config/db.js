fac2f85dc4019a291ceca7e0059a1a3c8d0e16ee
sangumee <super2451894@gmail.com>
2018-06-06 22:16:28 +0900

    DB Modify
    DB Modify

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ module.exports = function() {
   // Connect OrientDB Database
   var OrientDB = require('orientjs');
   var server = OrientDB({
-    host: 'sangumserver.iptime.org',
+    host: 'pandoracube.iptime.org',
     port: 2424,
     username: 'root',
     password: 'password123' // Bad Method
